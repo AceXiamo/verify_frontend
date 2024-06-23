7e15bb905ae8845a25fe6bfc343b9ae4e03ecd81
AceXiamo <xiamo@qwq.link>
2024-06-24 02:51:42 +0800

    refactor: remove i18n

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,15 +1,5 @@
 <script setup lang="ts">
-import { availableLocales, loadLanguageAsync } from '~/modules/i18n'
-
-const { t, locale } = useI18n()
-
-async function toggleLocales() {
-  // change to some real logic
-  const locales = availableLocales
-  const newLocale = locales[(locales.indexOf(locale.value) + 1) % locales.length]
-  await loadLanguageAsync(newLocale)
-  locale.value = newLocale
-}
+const { t } = useI18n()
 </script>
 
 <template>
@@ -21,10 +11,6 @@ async function toggleLocales() {
     <button icon-btn :title="t('button.toggle_dark')" @click="toggleDark()">
       <div i="carbon-sun dark:carbon-moon" />
     </button>
-
-    <a icon-btn :title="t('button.toggle_langs')" @click="toggleLocales()">
-      <div i-carbon-language />
-    </a>
 
     <RouterLink icon-btn to="/about" :title="t('button.about')" data-test-id="about">
       <div i-carbon-dicom-overlay />
