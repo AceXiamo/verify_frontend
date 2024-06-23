14b7549560d1480c1483af5d6b713e41c7257d1b
AceXiamo <xiamo@qwq.link>
2024-06-24 02:52:34 +0800

    refactor: remove github

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -15,9 +15,5 @@ const { t } = useI18n()
     <RouterLink icon-btn to="/about" :title="t('button.about')" data-test-id="about">
       <div i-carbon-dicom-overlay />
     </RouterLink>
-
-    <a icon-btn rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank" title="GitHub">
-      <div i-carbon-logo-github />
-    </a>
   </nav>
 </template>
